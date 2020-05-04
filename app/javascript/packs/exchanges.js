d937f8d5688a4066d5c05b4402b98105241b4e82
ReinaldoACDC <reinaldoacdc@gmail.com>
2020-05-04 09:33:46 -0300

    Desafio - parte1/2

--- app/javascript/packs/exchanges.js
+++ app/javascript/packs/exchanges.js
@@ -1,6 +1,30 @@
 document.addEventListener('turbolinks:load', function(){
-    document.getElementById('exchange_form').addEventListener('ajax:success', function(event) {
-      let [result] = event.detail
-      document.getElementById('result').value = result.value
+    document.getElementById('amount').addEventListener('change', function(event) {      
+      let source_currency = document.getElementById('source_currency').value
+      let target_currency = document.getElementById('target_currency').value
+      let amount = document.getElementById('amount').value
+
+       
+      let url = new URL("http:localhost:3000/convert")
+      let params = {source_currency: source_currency
+        , target_currency: target_currency
+        , amount: amount }
+      Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+  
+      fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => document.getElementById('result').value = data.value);
     })
+
+    document.getElementById('inverter').addEventListener('click', function(event) {
+      console.log('inverter')
+      let source_currency = document.getElementById('source_currency').value
+      let target_currency = document.getElementById('target_currency').value
+
+      document.getElementById('source_currency').value = target_currency
+      document.getElementById('target_currency').value = source_currency
+    })
+
   })
